301553147495d574a741b806eea8ffba5cd15c49
posubabupilla <108345262+posubabupilla@users.noreply.github.com>
2023-02-01 19:38:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function updateClock(){
     let h = new Date().getHours()
     let m = new Date().getMinutes()
     let s = new Date().getSeconds()
-    let ampm = "PM"
+    let ampm = "AM"
     
     if (h > 12) 
     {
